386ae331b2a40439718719c0d2a9bcad44b45111
established001 <blaccvibes@gmail.com>
2022-10-10 23:24:04 -0700

    started working with JS

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// const myImages = document.querySelector('.one');
+// const zoomBtn = document.querySelector('.zoomOne');
+// const overlay = document.querySelector('.overlay');
+
+// myImages.addEventListener('mouseover', function(){
+//     zoomBtn.classList.remove('zoom1');
+// })
+
+// myImages.addEventListener('mouseleave', function(){
+//     zoomBtn.classList.add('zoom1');
+// })
+
+// zoomBtn.addEventListener('click', function(){
+//     overlay.classList.remove('hidden');
+// })
+
